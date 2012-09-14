5fe81fdc0d171ae242d8ebd44a26cba6ba6f1946
Manu Raghavan <manu@raghavan.info>
2012-09-14 02:50:58 +0000

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fleece::Application.config.secret_token = '4ba657f466be061b60c65de18f349364975ffcd58708eda44fcee45090b85bd3407cddce11d367f122a44d50db9a9d8475e07e1ab0f1480a660fa13e29372a12'
