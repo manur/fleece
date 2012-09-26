0cdfc2465f2414b5c3d290cef39661e2f7831b04
Manu Raghavan <manu@raghavan.info>
2012-09-26 02:07:37 +0000

    BAckend end data persistence, not used yet

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,9 +1,21 @@
+require 'json'
 class Datum < ActiveRecord::Base
 
   belongs_to :user_account
   has_and_belongs_to_many :drawings
 
-  def json
+  validates :content, :presence => true
+  validate :content_is_json?
+
+  def content_is_json?
+    begin
+      JSON.parse(self.content || '')
+    rescue JSON::ParserError
+      self.errors.add(:content, "is not valid JSON")
+    end
+  end
+
+  def to_json
     content.to_json
   end
 end
