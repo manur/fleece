7e75c3418742af30dc9099d32c32bd31508a198b
Manu Raghavan <manu@raghavan.info>
2012-09-26 00:54:00 +0000

    Routes, info controller, etc.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require google_analytics
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Fleece::Application.routes.draw do
+  get "info/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
